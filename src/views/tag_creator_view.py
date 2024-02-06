69713063af5d57599a31e20cbb8426707ede2559
Spedine <tucogamesytthegamesof@gmail.com>
2024-02-06 16:48:42 -0300

    feat: Adding Controllers and Handlers

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,16 +1,21 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
+
 
 class TagCreatorView:
     '''
         responsable for HTTP interaction
     '''
     def validade_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
 
         # business logic rule
-        print("Estou na view")
+        formatted_response = tag_creator_controller.create(product_code=product_code)
+
         # return http
 
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
